e5bb9201764d4c290330d1381e17967837913e97
beto6 <beto6@DESKTOP-CBL37F1>
2022-04-11 20:17:36 -0600

    Add project files.

---
+++ Milestone2, CST150.2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Roberto GonzalezRamirez
+//Class CST-150
+
+namespace Milestone2
+{
+    internal class InventoryClass
+    {
+        //Defined properties
+        public string VehicleYear { get; set; }
+        public string VehicleMake { get; set; }
+        public string VehicleModel { get; set; }
+        public string VehicleColor { get; set; }
+        public double VehiclePrice { get; set; }
+        public string VehicelDescription { get; set; }
+
+        //Driver
+        public InventoryClass(string year, string make, string model, string color, double price, string description)
+        {
+            this.VehicleYear = year;
+            this.VehicleMake = make;
+            this.VehicleModel = model;
+            this.VehicleColor = color;
+            this.VehiclePrice = price;
+            this.VehicelDescription = description;
+        }
+
+
+    }
+}
